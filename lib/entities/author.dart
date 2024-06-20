12a1a50da233f128926f5aad5bf4f2dde520c258
DEBORA DAREN PEREIRA <debora.5974@aluno.pr.senac.br>
2024-06-19 21:06:42 -0300

    Criado arquivo author.

---
+++ lib/entities/author.dart
@@ -0,0 +1,15 @@
+//LATE - Modificador de variáveis
+
+
+/* CLASSE AUTHOR */
+class Author {
+  BigInt id;
+  String name;
+  String email;
+
+  Author({
+    required this.id,
+    required this.name,
+    required this.email
+  });
+}
