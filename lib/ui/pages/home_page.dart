50e6803f87e9e409f47f15f30d0ad329a6a115a7
Matheus <pereiraneto0130@gmail.com>
2024-06-26 20:20:43 -0300

    Criado tela inicial

---
+++ lib/ui/pages/home_page.dart
@@ -0,0 +1,16 @@
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
